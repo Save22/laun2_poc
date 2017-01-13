0cbb672e2c3a436f6a011ad662b85c783dd466f0
vnix <vlouie00@gmail.com>
2017-01-13 20:40:26 +0800

    LAUN2-8: Inherit Page model to app model and registered to Admin

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from mezzanine.pages.admin import PageAdmin
+from .models import About
 
-# Register your models here.
+admin.site.register(About, PageAdmin)
--- about/models.py
+++ about/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from mezzanine.pages.models import Page, RichText
 
-# Create your models here.
+
+class About(Page, RichText):
+    pass
