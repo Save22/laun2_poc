ab575a418457cc2a2b5c8fc78cb2e44616b11183
vnix <vlouie00@gmail.com>
2017-01-20 20:57:40 +0800

    LAUN2-7 Modified views.py to get all promos from Promo Model

--- promo_list/views.py
+++ promo_list/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import View
+from promo.models import Promo
 
-# Create your views here.
+
+class Promos(View):
+    template_name = 'promo_list/index.html'
+
+    def get_context_data(self):
+        context = {
+            'promos': Promo.objects.published()
+        }
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
