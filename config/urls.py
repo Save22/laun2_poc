b2964c8c5f249fe6bb95c8fa74f9a9db24289ad2
vnix <vlouie00@gmail.com>
2017-01-25 12:05:56 +0800

    LAUN2-7 Registered url routing of app to project

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ urlpatterns = i18n_patterns(
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
     url("^admin/", include(admin.site.urls)),
+    url("", include("promo_list.urls")),
 )
 
 if settings.USE_MODELTRANSLATION:
